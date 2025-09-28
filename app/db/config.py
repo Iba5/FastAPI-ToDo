afe9124fea93f33f19840bf943a96fef45b7805a
Iba5 <kariwoibanoshie@gmail.com>
2025-09-28 12:10:06 +0530

    Explanation

--- app/db/config.py
+++ app/db/config.py
@@ -8,3 +8,17 @@ class Settings(BaseSettings):
     )
 
 Config = Settings() # type: ignore
+
+
+"""
+BaseSettings:
+-> We import BaseSettings from Pydantic.
+-> Any class that inherits from BaseSettings automatically gains the ability to read values from environment files (like .env).
+-> This tells us that the class is meant to hold configuration settings, such as database credentials.
+-> We then create an instance of the class, which loads the database URL and other secrets we’ve hidden in the .env file.
+
+SettingsConfigDict:
+-> We import SettingsConfigDict to configure how Pydantic reads environment settings.
+-> It helps locate the .env file where we store secret values (e.g., database URLs) so they aren’t exposed in the code.
+-> It also clarifies which environment is being used and defines behaviors like ignoring extra fields. 
+"""
